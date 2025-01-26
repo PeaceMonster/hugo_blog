52cf0cb3ec027ffec3d60e2e13594f18b07866c9
Mikkel Ugilt <ugilt3@gmail.com>
2025-01-26 16:55:30 +0100

    Setting up flake for dev env

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Hugo based blog";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
